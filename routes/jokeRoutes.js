2204651486818ff88532b082dc9b4c399a806e52
AlineCoatanoan <aline.coatanoan@gmail.com>
2025-07-22 10:55:43 +0200

    setup routes

--- routes/jokeRoutes.js
+++ routes/jokeRoutes.js
@@ -1,4 +1,9 @@
 import express from "express";
+import { getAllJokes, getJokeById, getRandomJoke, createJoke } from "../controllers/jokeController.js";
 
 export const jokeRoutes = express.Router();
 
+jokeRoutes.get("/jokes", getAllJokes);
+jokeRoutes.get("/jokes/random", getRandomJoke);
+jokeRoutes.get("/jokes/:id", getJokeById);
+jokeRoutes.post("/jokes", createJoke);
