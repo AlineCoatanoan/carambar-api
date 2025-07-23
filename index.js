caf8a553b3467b70e4b0a0514bdb66537e338b6e
AlineCoatanoan <aline.coatanoan@gmail.com>
2025-07-23 11:34:33 +0200

    change url front

--- index.js
+++ index.js
@@ -11,7 +11,7 @@ const app = express();
 app.use(express.json());
 
 app.use(cors({
-  origin: ['https://alinecoatanoan.github.io', 'http://alinecoatanoan.github.io']
+  origin: ['https://alinecoatanoan.github.io/carambar-front']
 }));
 
 const swaggerDocument = JSON.parse(fs.readFileSync(path.resolve("./swagger.json"), "utf8"));
